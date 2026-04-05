abe4910cce171f10fa55b3d009d309b63604b4d8
Matej Knopp <matej.knopp@gmail.com>
2026-04-05 18:02:51 +0200

    Windows: Prepare for multiwindow

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -1,8 +1,8 @@
 #include <flutter/dart_project.h>
 #include <flutter/flutter_view_controller.h>
+#include <flutter/generated_plugin_registrant.h>
 #include <windows.h>
 
-#include "flutter_window.h"
 #include "utils.h"
 
 int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
@@ -19,18 +19,13 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
 
   flutter::DartProject project(L"data");
 
-  std::vector<std::string> command_line_arguments =
-      GetCommandLineArguments();
+  std::vector<std::string> command_line_arguments = GetCommandLineArguments();
 
   project.set_dart_entrypoint_arguments(std::move(command_line_arguments));
 
-  FlutterWindow window(project);
-  Win32Window::Point origin(10, 10);
-  Win32Window::Size size(1280, 720);
-  if (!window.Create(L"custom_window", origin, size)) {
-    return EXIT_FAILURE;
-  }
-  window.SetQuitOnClose(true);
+  auto const engine{std::make_shared<flutter::FlutterEngine>(project)};
+  RegisterPlugins(engine.get());
+  engine->Run();
 
   ::MSG msg;
   while (::GetMessage(&msg, nullptr, 0, 0)) {
